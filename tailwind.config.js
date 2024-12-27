00167411890e2ab5e4916c4601514ca5bfdfed26
zakariaeelhassad <zakariaelhassad031@gmail.com>
2024-12-27 09:00:06 +0100

    delete des image

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'stade-img': "url('/img/tiran.png')",
-        'main-img': "url('/img/c.jpg')",
-        'carte-img':"url('/img/carte2.jpg')"
-    },
+   
   },
   plugins: [],
 }
